f4507d35738b205cbb5612b2414ac63689d8369c
Ishi <162271161+ishi-6@users.noreply.github.com>
2025-08-15 07:11:20 -0400

    MLProject

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# scripts/__init__.py
-from ..preprocess import preprocess_code
-from ..ml_optimizer import CodeOptimizerDQL
-from .benchmark import benchmark_performance
-
--- sample_code.py
+++
@@ -1,4 +0,0 @@
-words = ["hello", "world", "test"]
-sentence = ""
-for word in words:
-    sentence += word + " "
